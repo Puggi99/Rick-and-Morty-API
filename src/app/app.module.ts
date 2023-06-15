1ad996a92aa54663b7af8f9475558c0dab57e29b
Puggi99 <puggionidaniele99@gmail.com>
2023-06-15 11:07:35 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import { ComponentCharacter } from './components/character/character.component';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatCardModule,
     MatInputModule,
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
