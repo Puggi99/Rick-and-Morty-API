5a5b56102d8654f8caf64fd4d11e0438f09170f3
Puggi99 <puggionidaniele99@gmail.com>
2023-06-16 15:24:11 +0200

    ddd

--- src/app/model/location.ts
+++ src/app/model/location.ts
@@ -1,4 +1,11 @@
 export interface Location {
+  id: number
+  name: string
+  type: string
+  dimension: string
+  residents: string[]
+  url: string
+  created: string
   info: Info
   results: Result[]
 }
